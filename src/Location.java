bb2714ae11d476fb6c37ed3cdcd38968880707be
Tony Reiter <treiter@vt.edu>
2014-04-14 11:26:48 -0400

    added javadocs and comments to everything

--- src/Location.java
+++ src/Location.java
@@ -1,21 +1,72 @@
-
+/**
+ * // -------------------------------------------------------------------------
+ * /** A location class that stores an x and y coordinate.
+ *
+ * @author Tony
+ * @version Apr 14, 2014
+ */
 public class Location
 {
     private int x, y;
-    public Location(int x, int y) {
+
+
+    /**
+     * Constructor for the location.
+     *
+     * @param x
+     *            the x coordinate
+     * @param y
+     *            the y coordinate
+     */
+    public Location(int x, int y)
+    {
         this.x = x;
         this.y = y;
     }
-    public int getX() {
+
+
+    /**
+     * Returns the x coordinate
+     *
+     * @return the x coordinate
+     */
+    public int getX()
+    {
         return x;
     }
-    public int getY() {
+
+
+    /**
+     * Returns the y coordinate
+     *
+     * @return the y coordinate
+     */
+    public int getY()
+    {
         return y;
     }
-    public void setX(int x) {
+
+
+    /**
+     * Changes the x coordinate to the input
+     *
+     * @param x
+     *            the new x coordinate
+     */
+    public void setX(int x)
+    {
         this.x = x;
     }
-    public void setY(int y) {
+
+
+    /**
+     * Changes the y coordinate to the input
+     *
+     * @param y
+     *            the new y coordinate
+     */
+    public void setY(int y)
+    {
         this.y = y;
     }
 }
--- src/Snake.java
+++ src/Snake.java
@@ -1,22 +1,58 @@
 import java.util.LinkedList;
 
-
+/**
+ * // -------------------------------------------------------------------------
+ * /** Snake class for the game. Uses a linked list to store locations, cause a
+ * snake is list-like. If the snake doesn't eat food, add a new head, and remove
+ * the tail. If it does eat food, don't remove the tail (so length increases).
+ *
+ * @author Tony
+ * @version Apr 14, 2014
+ */
 public class Snake
 {
     private LinkedList<Location> snakeList = new LinkedList<Location>();
-    private int[][] backgroundMatrix;
-    public Snake(int[][] backgroundMatrix) {
-        this.backgroundMatrix = backgroundMatrix; //should create a shallow copy
-        //TODO: make sure this works
-        int x = backgroundMatrix.length/2;
-        int y = backgroundMatrix[x].length/2;
+    private int[][]              backgroundMatrix;
+
+
+    /**
+     * Creates a new snake object, with a pointer matrix to the logicMatrix from
+     * game. Because matrix1 = matrix2 creates a shallow copy, this will make
+     * that updating the matrix in either class (game or snake) will update it
+     * in both.
+     *
+     * @param backgroundMatrix
+     *            a integer matrix containing the locations of everything. 0 is
+     *            blank, 1 is food, 2 is a snake part
+     */
+    public Snake(int[][] backgroundMatrix)
+    {
+        this.backgroundMatrix = backgroundMatrix; // should create a shallow
+                                                  // copy
+        int x = backgroundMatrix.length / 2;
+        int y = backgroundMatrix[x].length / 2;
         backgroundMatrix[x][y] = 2;
         snakeList.addFirst(new Location(x, y));
     }
-    public int move(int direction) {
+
+
+    /**
+     * To be called every time the snake moves. Moves the head forward in the
+     * correct direction, and if it eats food, increases the length by one.
+     * Checks to see if the snake is out of bounds, or if it has hit itself.
+     *
+     * @param direction
+     *            the direction to move in (1=up, 2=right, 3=down, 4=left).
+     * @return a status indicator. 0 means the snake just moved with no
+     *         complications, 1 means the snake ate food, and 2 means the snake
+     *         died //TODO: make it return 2 instead of just exiting
+     */
+    public int move(int direction)
+    {
         Location head = snakeList.getFirst();
         Location newHead = new Location(head.getX(), head.getY());
-        switch(direction) {
+        switch (direction)
+        {
             case 1:
                 newHead.setY(newHead.getY() - 1);
                 break;
@@ -33,20 +69,26 @@ public class Snake
                 break;
         }
         int returnStatus = 0;
-        if(newHead.getX() >= backgroundMatrix.length || newHead.getX() < 0)
+        // check to see if it died
+        if (newHead.getX() >= backgroundMatrix.length || newHead.getX() < 0)
             System.exit(1);
-        if(newHead.getY() >= backgroundMatrix[0].length || newHead.getY() < 0)
+        if (newHead.getY() >= backgroundMatrix[0].length || newHead.getY() < 0)
             System.exit(1);
-        if(backgroundMatrix[newHead.getX()][newHead.getY()] == 2)
+        if (backgroundMatrix[newHead.getX()][newHead.getY()] == 2)
             System.exit(1);
-        if(backgroundMatrix[newHead.getX()][newHead.getY()] == 0) {
+
+        // if it didn't eat food, remove the tail
+        if (backgroundMatrix[newHead.getX()][newHead.getY()] == 0)
+        {
             Location tail = snakeList.removeLast();
             backgroundMatrix[tail.getX()][tail.getY()] = 0;
         }
-        else {
+        else
+        {
             System.out.println(snakeList.size() + 1);
             returnStatus = 1;
         }
+        // add the new head, update where it is, and return the correct status
         snakeList.addFirst(newHead);
         backgroundMatrix[newHead.getX()][newHead.getY()] = 2;
         return returnStatus;
